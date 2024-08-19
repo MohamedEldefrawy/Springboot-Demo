38c71c618d55c4acc939827328e97dae80641741
Mohamed Eldafrawy <mohamed.eldafrawy@vodafone.com>
2024-08-19 17:51:03 +0200

    override default Authentication behavior

--- src/main/java/com/vodafone/onionpatterndemo/config/SecurityConfig.java
+++ src/main/java/com/vodafone/onionpatterndemo/config/SecurityConfig.java
@@ -2,13 +2,15 @@ package com.vodafone.onionpatterndemo.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -23,5 +25,13 @@ public class SecurityConfig {
   @Bean
   public PasswordEncoder getPasswordEncoder() {
     return NoOpPasswordEncoder.getInstance();
+  }
+
+  @Bean
+  public SecurityFilterChain getSecurityFilterChain(HttpSecurity http) throws Exception {
+    http.httpBasic(Customizer.withDefaults());
+    http.authorizeHttpRequests(authorizationManagerRequestMatcherRegistry ->
+        authorizationManagerRequestMatcherRegistry.anyRequest().permitAll());
+    return http.build();
   }
 }
